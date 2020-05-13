625ddce83f4733daca3445923b37ddc09fe09613
James Lloyd <jl2009@camac.uk>
2020-05-13 17:20:58 +0100

    UX for photo adding sorted, need to do backend

--- lib/components/add_image_screen.dart
+++ lib/components/add_image_screen.dart
@@ -8,13 +8,14 @@ import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-
 /* TODO: things left to do here:
     -design 3 scenarios (has image, doesn't have image, added/changed image)
     -store file to correct location
     -handle timeout/failure errors
     -passing in user's current image as placeholder
  */
+
+bool noChangesMade = true;
 
 Color _iconColor = Color(kGenchiCream);
 
@@ -30,22 +31,29 @@ class _AddImageScreenState extends State<AddImageScreen> {
     File selected = await ImagePicker.pickImage(source: source);
 
     setState(() {
+      if(selected!=null) noChangesMade = false;
       _imageFile = selected;
     });
   }
 
   //Remove image
   void _clear() {
-    setState(() => _imageFile = null);
+    //TODO: need to change this so that any existing image is passed back to avatar
+
+    setState(() {
+      noChangesMade = true;
+      _imageFile = null;
+    });
   }
 
   //Croper plugin
   Future<void> _cropImage() async {
+
     File cropped = await ImageCropper.cropImage(
         sourcePath: _imageFile.path,
         cropStyle: CropStyle.circle,
         compressFormat: ImageCompressFormat.png,
-        //TODO: check to see what this looks like
+        //TODO: MUST check to see what this looks like
         androidUiSettings: AndroidUiSettings(
           toolbarColor: Colors.purple,
           toolbarWidgetColor: Colors.white,
@@ -56,6 +64,7 @@ class _AddImageScreenState extends State<AddImageScreen> {
         ));
 
     setState(() {
+        if(cropped!=null) noChangesMade = false;
       _imageFile = cropped ?? _imageFile;
     });
   }
@@ -94,93 +103,100 @@ class _AddImageScreenState extends State<AddImageScreen> {
           ),
           Container(
             height: MediaQuery.of(context).size.height * 0.75 - 130,
-//            color: Color(kGenchiCream),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                if (_imageFile != null) ...[
-                  CircleAvatar(
-                    radius:
-                        (MediaQuery.of(context).size.height * 0.75 - 130) * 0.35,
-                    backgroundImage: FileImage(_imageFile),
-                    backgroundColor: Colors.transparent,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      IconButton(
-                        color: _iconColor,
-                        iconSize: 25,
-                        icon: Icon(Icons.crop),
-                        onPressed: _cropImage,
+                _imageFile == null
+                    ? CircleAvatar(
+                        radius: (MediaQuery.of(context).size.height * 0.75 - 130) * 0.35,
+                        backgroundColor: Color(kGenchiCream),
+                      )
+                    : CircleAvatar(
+                  //ToDo, existing image, this should probably be stored on the device, only if one exists!
+                        backgroundImage: FileImage(_imageFile),
+                        radius: (MediaQuery.of(context).size.height * 0.75 - 130) * 0.35,
+                        backgroundColor: Color(kGenchiCream),
                       ),
-                      Uploader(
-                        file: _imageFile,
-                      ),
-                    ],
+                SizedBox(
+                  height: 30,
+                  child: Center(
+                    child: IconButton(
+                      color: _iconColor,
+                      iconSize: 25,
+                      icon: Icon(Icons.crop),
+                      onPressed: _imageFile == null ? (){}: _cropImage,
+                    ),
                   ),
-                ] else ...[
-                  CircleAvatar(
-                    radius:
-                    (MediaQuery.of(context).size.height * 0.75 - 130) * 0.35,
-                    backgroundColor: Color(kGenchiCream),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Icon(
-                        Icons.crop,
-                        size: 25,
-                        color: _iconColor,
-                      ),
-                      //Todo: add placeholder here
-                      Uploader(
-                        file: _imageFile,
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ],
             ),
           ),
-          Container(
+          SizedBox(
             height: 40,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                IconButton(
-                  icon: Icon(
-                    Platform.isIOS
-                        ? CupertinoIcons.photo_camera_solid
-                        : Icons.photo_camera,
-                    size: 25,
+                SizedBox(
+                  width: (MediaQuery.of(context).size.width -40)/3,
+                  child: IconButton(
+                    icon: Icon(
+                      Platform.isIOS
+                          ? CupertinoIcons.photo_camera_solid
+                          : Icons.photo_camera,
+                      size: 25,
+                    ),
+                    onPressed: () => _pickImage(ImageSource.camera),
+                    color: _iconColor,
                   ),
-                  onPressed: () => _pickImage(ImageSource.camera),
-                  color: _iconColor,
                 ),
-                IconButton(
-                  icon: Icon(
-                    Platform.isIOS
-                        ? CupertinoIcons.collections_solid
-                        : Icons.photo_library,
-                    size: 25,
+                SizedBox(
+                  width: (MediaQuery.of(context).size.width -40)/3,
+                  child: IconButton(
+                    icon: Icon(
+                      Platform.isIOS
+                          ? CupertinoIcons.collections_solid
+                          : Icons.photo_library,
+                      size: 25,
+                    ),
+                    onPressed: () => _pickImage(ImageSource.gallery),
+                    color: _iconColor,
                   ),
-                  onPressed: () => _pickImage(ImageSource.gallery),
-                  color: _iconColor,
                 ),
-                IconButton(
-                  icon: Icon(
-                    Platform.isIOS ? CupertinoIcons.delete_solid : Icons.delete,
-                    size: 25,
-                  ),
-                  /*ToDo: add in functionality that asks if the user wants to delete
-                     their image and will clear the display and show standard image
+                noChangesMade
+                    ? SizedBox(
+                  width: (MediaQuery.of(context).size.width -40)/3,
+                      child: IconButton(
+                          icon: Icon(
+                            Platform.isIOS
+                                ? CupertinoIcons.delete_solid
+                                : Icons.delete,
+                            size: 25,
+                          ),
+                          /* ToDo: add in functionality that asks if the user wants to delete
+                             their image and will clear the display and show standard image
                    */
-                  //                  onPressed: _clear,
-                  onPressed: () {},
-                  color: _iconColor,
-                ),
+                          //                  onPressed: _clear,
+                          onPressed: () {},
+                          color: _iconColor,
+                        ),
+                    )
+                    : SizedBox(
+                  width: (MediaQuery.of(context).size.width -40)/3,
+                    child: Uploader(file: _imageFile)),
+
+//                IconButton(
+//                        icon: Icon(
+//                          Platform.isIOS
+//                              ? CupertinoIcons.clear_thick
+//                              : Icons.clear,
+//                          size: 25,
+//                        ),
+//                        //ToDo: add in functionality that asks if the user wants to reset changes
+//                        //TODO this has also got to cancel any upload if clicked!
+//                        onPressed: _clear,
+//                        color: _iconColor,
+//                      ),
               ],
             ),
           )
@@ -206,10 +222,11 @@ class _UploaderState extends State<Uploader> {
   StorageUploadTask _uploadTask;
 
   _startUpload() {
-    //TODO: change this
-    String filePath = 'images/${DateTime.now()}.png';
+    //TODO: change this to be the current users id
+    String filePath = 'images/users/${DateTime.now()}.png';
 
     setState(() {
+      noChangesMade = true;
       _uploadTask = _storage.ref().child(filePath).putFile(widget.file);
     });
   }
@@ -222,72 +239,79 @@ class _UploaderState extends State<Uploader> {
           builder: (context, snapshot) {
             var event = snapshot?.data?.snapshot;
 
-            //TODO: this doesn't work, but need to find a way to handle fails or timesout
-            if (!_uploadTask.isSuccessful){
+            //TODO: this doesn't work, but need to find a way to handle fails or timeouts
+            if (!_uploadTask.isSuccessful) {
               print('Uncessessful');
             }
-
             if (_uploadTask.isComplete) {
-              return SizedBox(
-                height:25,
-                width: 25,
-                child: Icon(
+              //TODO: must add file path to firestore
+              //TODO: must update current user here
+
+              print('Is complete');
+              return IconButton(
+                iconSize: 25,
+                color: _iconColor,
+                icon: noChangesMade
+                    ? Icon(
                   Platform.isIOS
                       ? CupertinoIcons.check_mark_circled_solid
                       : Icons.check_circle,
-                  color: _iconColor,
-                  size: 25,
+                )
+                    : Icon(
+                  Platform.isIOS
+                      ? CupertinoIcons.check_mark_circled
+                      : Icons.check_circle_outline,
                 ),
+                onPressed: noChangesMade ? () {} : _startUpload,
               );
             } else if (_uploadTask.isInProgress) {
-              return SizedBox(
-                height: 40,
-                child: Center(
-                  child: Container(
-                    height: 25,
-                    width: 25,
+              print('inProgress');
+              return FlatButton(
+                onPressed: _uploadTask.pause,
+                child: Container(
+                  height: 20,
+                  width: 20,
+                  child: Center(
                     child: CircularProgressIndicator(
 //                    value: progressPercent,
-                      valueColor:
-                          AlwaysStoppedAnimation<Color>(_iconColor),
+                      valueColor: AlwaysStoppedAnimation<Color>(_iconColor),
                       strokeWidth: 2.0,
                     ),
                   ),
                 ),
               );
-
+            } else if(_uploadTask.isPaused) {
+              return IconButton(
+                iconSize: 25,
+                color: _iconColor,
+                icon: Icon(
+                  Platform.isIOS
+                      ? CupertinoIcons.check_mark_circled
+                      : Icons.check_circle_outline,
+                ),
+                onPressed: _uploadTask.resume,
+              );
             }
 
             return Text("");
 
-            //TODO: MUST add file path to firestore and update current user
-
-            //ToDo: implement this later
-//                  if (_uploadTask.isPaused)
-//                    FlatButton(
-//                      child: Icon(Icons.play_arrow, size: 30),
-//                      onPressed: _uploadTask.resume,
-//                    ),
-//                  if (_uploadTask.isInProgress)
-//                    FlatButton(
-//                      child: Icon(Icons.pause, size: 30),
-//                      onPressed: _uploadTask.pause,
-//                    ),
-
-
           });
     } else {
-      return Container(
-        child: IconButton(
-          iconSize: 25,
-          color: _iconColor,
-          icon: Icon(
-            Platform.isIOS
-                ? CupertinoIcons.check_mark_circled
-                : Icons.check_circle_outline,
-          ),
-          onPressed: _startUpload,
-        ),
+      return IconButton(
+        iconSize: 25,
+        color: _iconColor,
+        icon: noChangesMade
+            ? Icon(
+                Platform.isIOS
+                    ? CupertinoIcons.check_mark_circled_solid
+                    : Icons.check_circle,
+              )
+            : Icon(
+                Platform.isIOS
+                    ? CupertinoIcons.check_mark_circled
+                    : Icons.check_circle_outline,
+              ),
+        onPressed: noChangesMade ? () {} : _startUpload,
       );
     }
   }
